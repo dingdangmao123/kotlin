61ab24cc0116d3ccdfe134a250389f75f80ef4a9
chuhan2014 <3228936343@qq.com>
2018-01-26 10:52:51 +0800

    new photo site

--- Coroutine/Coroutine.kt
+++ Coroutine/Coroutine.kt
@@ -13,37 +13,34 @@ import java.net.URL
 
 fun main(args: Array<String>)=runBlocking {
 
-    var num=30
+    var num=50
     var cache=100
-    var dir="G:\\Hub"
-    var page=10
+    var dir="G:\\Hub\\wallpaper"
+    var page=21
 
     var ch=Channel<String>(cache)
     var job= mutableListOf<Job>()
-
 
     for(i in 1..num)
         job.add(launch(CommonPool) {
             for(i in ch)
                 File(dir+File.separator+i.split("/").last()).writeBytes(URL(i).readBytes())
             })
-
     var count=0
     for(i in 1..page) {
-        var pos="https://pixabay.com/zh/photos/?order=ec&pagi=$i"
+        var j=(i-1)*24
+        var pos="https://www.vladstudio.com/zh/wallpapers/?skip=$j"
         println(pos)
-        for (j in Regex("src=\"(http[^\"]+\\.(png|jpg|jpeg))\"").findAll(URL(pos).readText())) {
+        var p="<img class=\"framed\" src=\"(http[^\"]+)\""
+        for (j in Regex(p).findAll(URL(pos).readText())) {
             count++
-            ch.send(j.groupValues.get(1))
+            ch.send(j.groupValues.get(1).replace("480x320","1920x1080"))
         }
-        println(count)
     }
-
     ch.close()
-    println(count)
+    println("total: "+count)
     for(i in job)
         i.join()
-
     println("done")
 }
 
